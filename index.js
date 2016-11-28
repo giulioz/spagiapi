291c2a8bf24cabf595909df4777c76605694827f
root <root@server-netvista.aldomoro.org>
2016-11-28 11:49:22 +0100

    Added multiple accounts support and school recognition.

--- index.js
+++ index.js
@@ -17,28 +17,45 @@ module.exports.login = function(username, password, callback) {
 	function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var auth = JSON.parse(body).data.auth;
-			
-			var ret = [];
-			ret.loggedIn	= auth.loggedIn;
-			ret.accountType	= auth.accountInfo.type;
-			ret.id		= auth.accountInfo.id;
-			ret.surname	= auth.accountInfo.cognome;
-			ret.name	= auth.accountInfo.nome;
-			
-			if (ret.loggedIn) {
-				request.get({url: regclasseUrl, jar: cookieJar},
-				function (rcerror, rcresponse, rcbody) {
-					if (!rcerror && rcresponse.statusCode == 200) {
-						ret.class = rcbody.match(/[1-5]+[A-Z]+/g)[0].substring(0,2);
-						callback(ret, error, response, body);
-					}
+
+			// If there are more than one student per account
+			if (auth.verified && !auth.loggedIn) {
+				var pfolio = JSON.parse(body).data.pfolio;
+				var ret = [];
+				ret.selection	= true;
+				ret.users = [];
+				pfolio.fullList.forEach(function(item,i){
+					ret.users.push({name: item.nome, uid: item.account_string});
 				});
-			} else {
-				callback(ret, error, response, body);
+				callback(ret, error, response);
+			}
+			// Only one user
+			else {
+				var ret = [];
+				ret.verified	= auth.verified;
+				ret.loggedIn	= auth.loggedIn;
+				ret.accountType	= auth.accountInfo.type;
+				ret.id			= auth.accountInfo.id;
+				ret.surname		= auth.accountInfo.cognome;
+				ret.name		= auth.accountInfo.nome;
+				
+				if (ret.loggedIn) {
+					request.get({url: regclasseUrl, jar: cookieJar},
+					function (rcerror, rcresponse, rcbody) {
+						if (!rcerror && rcresponse.statusCode == 200) {
+							ret.class = rcbody.match(/[1-5]+[A-Z]+/g)[0].substring(0,2);
+							ret.school = rcbody.match(/<span class=\"scuola\">(.*?)<\/span>/g)[0];
+							ret.school = ret.school.substring(21, ret.school.length - 7);
+							callback(ret, error, response);
+						}
+					});
+				} else {
+					callback(null, error, response);
+				}
 			}
 		}
 		else {
-			callback(null, error, response, body);
+			callback(null, error, response);
 		}
 	})
 };
